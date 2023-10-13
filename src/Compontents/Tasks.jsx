38e6c46d55c19bdb5fe56fd824ea4079b3fd07c9
mariem <mariamkhaled709@gmail.com>
2023-10-13 18:04:01 +0300

    fixed styling issues

--- src/Compontents/Tasks.jsx
+++ src/Compontents/Tasks.jsx
@@ -8,7 +8,7 @@ export default function Tasks({tasks,toggle}) {
     <div className="checkbox-wrapper-11">
     <input id={tasks.id} type="checkbox" name="r" value="2" checked={tasks.complete} onChange={()=>toggle(tasks.id)}/>
       
-        {tasks.name}
+       {tasks.name}
 
     <label htmlFor={tasks.id}>To-do</label>
     </div>
